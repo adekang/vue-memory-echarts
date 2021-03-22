fbc25600adc230dde9fc6b55dcacd6ba7d31235f
TopLong <201497709@qq.com>
2021-03-22 20:36:56 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    Money.vue
-    <hr/>
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
+
     <Nav/>
   </div>
 </template>
@@ -13,5 +15,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
