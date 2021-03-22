4c0604829cfa04630c51f511b26e0d1854a1bc33
TopLong <201497709@qq.com>
2021-03-22 20:18:51 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>访问的页面不存在，煞笔</div>
+    <div>
+      <router-link to="/">点击返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style scoped>
+
+</style>
