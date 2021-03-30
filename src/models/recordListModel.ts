be1667b8e3ab86b3e9843622aa2b2708b4d16468
TopLong <201497709@qq.com>
2021-03-30 19:12:31 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,15 @@
 const localStorageName = 'recordListModel';
 
 const recordListModel = {
+  data: [] as RecordItem[],
 //  获取数据
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
-
+    this.data = JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
+    return this.data;
   },
   //保存数据
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageName, JSON.stringify(this.data));
   },
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
