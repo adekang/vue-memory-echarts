940c0acd982978a21bbfa9a21cda131e58c124b5
TopLong <201497709@qq.com>
2021-03-30 19:32:11 +0800

    使用全局 tagList

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -2,19 +2,7 @@ import createID from '@/lib/createID';
 
 const localStorageName = 'tagList';
 
-//声明数据类型
-type  Tag = {
-  id: string
-  name: string
-}
-type  TagListModel = {
-  data: Tag[]
-  fetch: () => Tag[]
-  create: (name: string) => 'success' | 'duplicated' // 联合类型
-  save: () => void
-  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
-  remove: (id: string) => boolean
-}
+
 const tagListModel: TagListModel = {
   data: [],
 //  获取数据
@@ -26,17 +14,17 @@ const tagListModel: TagListModel = {
   save() {
     window.localStorage.setItem(localStorageName, JSON.stringify(this.data));
   },
-  create(name:string) {
+  create(name: string) {
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) {
       return 'duplicated';
     }
-    const id = createID().toString()
+    const id = createID().toString();
     this.data.push({id, name: name});
     this.save();
     return 'success';
   },
-  update(id:string, name:string) {
+  update(id: string, name: string) {
     const idList = this.data.map(item => item.id);
     if (idList.indexOf(id) >= 0) {
       const names = this.data.map(item => item.name);
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -33,7 +33,7 @@ const tagList = tagListModel.fetch();
   components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList;
 
   // 收集相关区域的值
   recordList: RecordItem[] = recordList;
