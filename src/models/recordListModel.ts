6071227367efc390956a82d528544b9bee7a08bb
TopLong <201497709@qq.com>
2021-03-30 20:31:06 +0800

    封装record增加

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,27 +1,25 @@
 import clone from '@/lib/clone';
 
-const localStorageName = 'recordListModel';
+const localStorageKeyName = 'recordList';
 
 const recordListModel = {
   data: [] as RecordItem[],
-  create(record:RecordItem) {
+  create(record: RecordItem) {
     const record2: RecordItem = clone(record);
     record2.createdAt = new Date();
     this.data.push(record2);
+    this.save();
   },
 //  获取数据
   fetch() {
-    this.data = JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     return this.data;
   },
   //保存数据
   save() {
-    window.localStorage.setItem(localStorageName, JSON.stringify(this.data));
+    window.localStorage.setItem(localStorageKeyName,
+      JSON.stringify(this.data));
   },
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data));
-  }
-
 };
 
 export default recordListModel;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -21,12 +21,7 @@ import NumberPad from '@/components/money/NumberPad.vue';
 import Types from '@/components/money/Types.vue';
 import FormItem from '@/components/money/FormItem.vue';
 import Tags from '@/components/money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
-
-
-const recordList = recordListModel.fetch();
-
+import {Component} from 'vue-property-decorator';
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
@@ -35,7 +30,7 @@ export default class Money extends Vue {
   tags = window.tagList;
 
   // 收集相关区域的值
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -49,14 +44,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record);
+    window.createRecord(this.record);
   }
 
-  //  保存在localStorage
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
 }
 
 
