edd64ec5a738aeacb1adfa7f5253ebce3ebe13ec
TopLong <201497709@qq.com>
2021-03-30 20:54:59 +0800

    将所有数据封装到 store里面

---
+++ src/store/index2.ts
@@ -0,0 +1,31 @@
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
+
+const store = {
+  // record store
+  recordList: recordListModel.fetch(),
+  createRecord: (record: RecordItem) => recordListModel.create(record),
+
+  // tag 的增删改查
+  tagList: tagListModel.fetch(),
+  createTag: (name: string) => {
+    const message = tagListModel.create(name);
+    if (message === 'duplicated') {
+      window.alert('标签名重复了');
+    } else if (message === 'success') {
+      window.alert('添加成功');
+    }
+  },
+  removeTag: (id: string) => {
+    return tagListModel.remove(id);
+  },
+  updateTag: (id: string, name: string) => {
+    return tagListModel.update(id, name);
+
+  },
+  findTag(id: string) {
+    return this.tagList.filter(t => t.id === id)[0];
+  },
+};
+
+export  default  store
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,15 +22,16 @@ import Types from '@/components/money/Types.vue';
 import FormItem from '@/components/money/FormItem.vue';
 import Tags from '@/components/money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = window.tagList;
+  tags = store.tagList;
 
   // 收集相关区域的值
-  recordList = window.recordList;
+  recordList = store.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -44,7 +45,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    window.createRecord(this.record);
+    store.createRecord(this.record);
   }
 
 }
